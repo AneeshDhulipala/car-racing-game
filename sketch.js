06cde775bacca0e28f043cbd9d6cf4ae8cedba76
AneeshDhulipala <72055587+AneeshDhulipala@users.noreply.github.com>
2020-12-28 10:37:44 -0600

    Add files via upload
    I made camera width

--- sketch.js
+++ sketch.js
@@ -2,15 +2,17 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
-
-
+var allPlayers;
+var distance = 0;
 var database;
 
 var form, player, game;
-var allPlayers  
+
+var cars, car1, car2, car3, car4;
+
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -19,12 +21,11 @@ function setup(){
 
 
 function draw(){
-  if (gameState===1){
-    clear();
-    game.play()
+  if(playerCount === 4){
+    game.update(1);
   }
-  if (playerCount===4){
-    game.update(1)
-
+  if(gameState === 1){
+    clear();
+    game.play();
   }
 }
